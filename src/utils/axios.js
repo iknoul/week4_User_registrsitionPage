61beb855f2d61c0cc6e12a2438a8b844b5aba683
SHAMIL RAHMAN <rhmanshamil@gmail.com>
2024-08-30 22:16:54 +0530

    Delete src directory

--- src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios  from 'axios'
-
-const customAxios = axios.create({
-    baseURL:'http://localhost:8888',
-    // headers:{Authorization:`Bearer ${localStorage.getItem('token')}`}
-})
-
-export default customAxios
